a783a943d47bb22caf880e8291678cf0b3d27430
nosomeone <no.someone@outlook.com>
2015-03-27 16:05:07 +0800

    Commit Initial Changes

---
+++ AzureWebGit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebGit.Startup))]
+namespace AzureWebGit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
